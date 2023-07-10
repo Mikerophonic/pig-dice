58a4ed5232f8546f124da405a8b2d9c5a5c8c759
Mikerophonic <micgcon@gmail.com>
2023-07-09 17:31:53 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 
 let roundScore = 0;
 let totalScore = 0;
+let breakPoints = 100;
 
 function rollDice() {
     let rollNumber = Math.floor(Math.random() * 6 + 1);
     const currentPlayer = getCurrentPlayer();
     currentPlayer.roundScore = addScore(rollNumber);
+    checkWin();
     return rollNumber;
 }
-
-
 
 function addScore(number) {
     if (number !== 1) {
@@ -24,6 +24,7 @@ function addScore(number) {
 function hold() {
     const currentPlayer = getCurrentPlayer();
     currentPlayer.totalScore += roundScore;
+    checkWin();
     currentPlayer.roundScore = 0;
     nextTurn();
 }
@@ -37,10 +38,12 @@ function getCurrentPlayer() {
     return game.players[currentPlayerId];
 }
 
-function Game() {
-  this.players = {};
-  this.currentId = 0;
-  this.round = 1;
+class Game {
+  constructor(){
+    this.players = {};
+    this.currentId = 0;
+    this.round = 1;
+  }
 }
 
 Game.prototype.addPlayer = function(player) {
@@ -53,13 +56,18 @@ Game.prototype.assignId = function() {
   return this.currentId;
 };
 
-function Player(roundScore, totalScore) {
-  this.roundScore = 0;
-  this.totalScore = 0;
+class Player {
+    constructor(roundScore, totalScore) {
+        this.roundScore = 0;
+        this.totalScore = 0;
+    }
 }
 
-Player.prototype.gameCard = function() {
-  return "Player: " + this.id + " " + "Round: " + this.round + " " + "Round Score: " + this.roundScore + " " + "Total Score: " + this.totalScore
+function checkWin() {
+  const currentPlayer = getCurrentPlayer();
+  if (breakPoints <= currentPlayer.totalScore || breakPoints <= currentPlayer.totalScore + currentPlayer.roundScore) {
+    return console.log("Winner")    
+  }
 }
 
 const game = new Game();
@@ -68,71 +76,32 @@ const player2 = new Player()
 game.addPlayer(player1);
 game.addPlayer(player2);
 
-// function rollDice() {
-//   let rollNumber = Math.floor(Math.random() * 6 + 1);
-//   return rollNumber;
+
+//UI Logic
+
+
+
+window.addEventListener("load", function () {
+    const rollAgainBtn = document.getElementById("rollAgainBtn");
+    const holdBtn = document.getElementById("holdBtn");
+    const totalScore1Span = document.getElementById("totalScore1");
+    const roundScore1Span = document.getElementById("roundScore1");
+    const totalScore2Span = document.getElementById("totalScore2");
+    const roundScore2Span = document.getElementById("roundScore2");
+    const player1Div = document.getElementById("player1")
+    const player2Div = document.getElementById("player2")
+    rollAgainBtn.addEventListener("click", rollDice);
+    holdBtn.addEventListener("click", hold);
+});
+
+// function highlightPlayer() {
+//     if (((game.round - 1) % Object.keys(game.players).length + 1) === 1) {
+//         player2Div.removeAttribute("class", "hidden")
+//     } else {
+//         player1Div.removeAttribute("class", "hidden")
+//     }
 // }
 
-// let roundScore = 0;
-// let totalScore = 0;
+// highlightPlayer()
 
-// function addScore(number) {
-//   if (number !== 1) {
-//     roundScore += number;
-//   } else {
-//     roundScore = 0;
-//   }
-//   return roundScore;
-// }
-
-// function hold() {
-//   totalScore += roundScore;
-//   roundScore = 0;
-// }
-
-// function nextTurn() {
-//   roundScore = 0;
-// }
-
-// function getCurrentPlayer() {
-//   const currentPlayerId = (game.round % Object.keys(game.players).length);
-//   return game.players[currentPlayerId];
-// }
-
-// function Game() {
-//   this.players = {};
-//   this.currentId = 0;
-//   this.round = 1;
-// }
-
-// Game.prototype.addPlayer = function(player) {
-//   player.id = this.assignId();
-//   this.players[player.id] = player;
-// };
-
-// Game.prototype.assignId = function() {
-//   this.currentId += 1;
-//   return this.currentId;
-// };
-
-// function Player(roundScore, totalScore) {
-//   this.roundScore = roundScore;
-//   this.totalScore = totalScore;
-// }
-
-// Player.prototype.gameCard = function() {
-//   return "Player: " + this.playerId + " " + "Round: " + this.round + " " + "Round Score: " + this.roundScore + " " + "Total Score: " + this.totalScore;
-// }
-
-// const game = new Game();
-// const player1 = new Player();
-// const player2 = new Player();
-// game.addPlayer(player1);
-// game.addPlayer(player2);
-
-// const currentPlayer = getCurrentPlayer();
-// currentPlayer.roundScore = rollDice();
-// currentPlayer.roundScore = addScore(rollDice());
-// currentPlayer.roundScore = addScore(rollDice());
-// hold();
-// console.log(currentPlayer.gameCard());
+// function displayScores
