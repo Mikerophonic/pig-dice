fecc66b966841a5aa5bf47459f2595802b969084
Mikerophonic <micgcon@gmail.com>
2023-07-10 20:55:56 -0700

    did a lot of stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 let roundScore = 0;
 let totalScore = 0;
 let breakPoints = 100;
@@ -6,25 +5,26 @@ let breakPoints = 100;
 function rollDice() {
     let rollNumber = Math.floor(Math.random() * 6 + 1);
     const currentPlayer = getCurrentPlayer();
-    currentPlayer.roundScore = addScore(rollNumber);
-    checkWin();
+    roundScore = addScore(rollNumber, currentPlayer);
+    checkWin(currentPlayer);
+    console.log(rollNumber);
     return rollNumber;
 }
 
-function addScore(number) {
+function addScore(number, currentPlayer) {
     if (number !== 1) {
-    roundScore += number;
+        currentPlayer.roundScore += number;
     } else {
-    roundScore = 0;
-    nextTurn();
-    }   
-    return roundScore;
+        currentPlayer.roundScore = 0;
+        nextTurn();
+    }
+    return currentPlayer.roundScore;
 }
 
 function hold() {
     const currentPlayer = getCurrentPlayer();
     currentPlayer.totalScore += roundScore;
-    checkWin();
+    checkWin(currentPlayer);
     currentPlayer.roundScore = 0;
     nextTurn();
 }
@@ -39,40 +39,39 @@ function getCurrentPlayer() {
 }
 
 class Game {
-  constructor(){
-    this.players = {};
-    this.currentId = 0;
-    this.round = 1;
-  }
+    constructor() {
+        this.players = {};
+        this.currentId = 0;
+        this.round = 1;
+    }
+
+    addPlayer(player) {
+        player.id = this.assignId();
+        this.players[player.id] = player;
+    }
+
+    assignId() {
+        this.currentId += 1;
+        return this.currentId;
+    }
 }
 
-Game.prototype.addPlayer = function(player) {
-  player.id = this.assignId();
-  this.players[player.id] = player;
-};
-
-Game.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-};
-
 class Player {
-    constructor(roundScore, totalScore) {
+    constructor() {
         this.roundScore = 0;
         this.totalScore = 0;
     }
 }
 
-function checkWin() {
-  const currentPlayer = getCurrentPlayer();
-  if (breakPoints <= currentPlayer.totalScore || breakPoints <= currentPlayer.totalScore + currentPlayer.roundScore) {
-    return console.log("Winner")    
-  }
+function checkWin(currentPlayer) {
+    if (breakPoints <= currentPlayer.totalScore || breakPoints <= currentPlayer.totalScore + currentPlayer.roundScore) {
+        console.log("Winner");
+    }
 }
 
 const game = new Game();
-const player1 = new Player()
-const player2 = new Player()
+const player1 = new Player();
+const player2 = new Player();
 game.addPlayer(player1);
 game.addPlayer(player2);
 
@@ -82,26 +81,49 @@ game.addPlayer(player2);
 
 
 window.addEventListener("load", function () {
-    const rollAgainBtn = document.getElementById("rollAgainBtn");
+    const rollAgainBtn = document.getElementById("rollBtn");
     const holdBtn = document.getElementById("holdBtn");
+    const startBtn = document.getElementById("startBtn");
     const totalScore1Span = document.getElementById("totalScore1");
     const roundScore1Span = document.getElementById("roundScore1");
     const totalScore2Span = document.getElementById("totalScore2");
     const roundScore2Span = document.getElementById("roundScore2");
-    const player1Div = document.getElementById("player1")
-    const player2Div = document.getElementById("player2")
-    rollAgainBtn.addEventListener("click", rollDice);
-    holdBtn.addEventListener("click", hold);
+    const player1Div = document.getElementById("1")
+    const player2Div = document.getElementById("2")
+    rollBtn.addEventListener("click", function() {
+      rollDice();
+      updateUI();
+    });
+    rollBtn.addEventListener('click', function() {
+        rollBtn.textContent = 'Roll Again';
+      });
+      rollBtn.addEventListener('mouseover', function() {
+        rollBtn.setattribute = ("style", "font-size: 32px");
+      });
+    holdBtn.addEventListener("click", function() {
+      hold();
+      updateUI();
+    });
+   
+
+    function updateUI() {
+    totalScore1Span.innerText = player1.totalScore;
+    totalScore2Span.innerText = player2.totalScore;
+    roundScore1Span.innerText = player1.roundScore;
+    roundScore2Span.innerText = player2.roundScore;
+    let cPlayer = getCurrentPlayer().id;
+
+    if (cPlayer === 1) {
+    document.getElementById(1).setAttribute("class", "player activePlayer");
+    document.getElementById(2).removeAttribute("class", "activePlayer")
+    document.getElementById(2).setAttribute("class", "player");
+    } else {
+      document.getElementById(2).setAttribute("class", "player activePlayer");
+      document.getElementById(1).removeAttribute("class", "activePlayer")
+      document.getElementById(1).setAttribute("class", "player");
+    }
+    
+    }
 });
 
-// function highlightPlayer() {
-//     if (((game.round - 1) % Object.keys(game.players).length + 1) === 1) {
-//         player2Div.removeAttribute("class", "hidden")
-//     } else {
-//         player1Div.removeAttribute("class", "hidden")
-//     }
-// }
 
-// highlightPlayer()
-
-// function displayScores
